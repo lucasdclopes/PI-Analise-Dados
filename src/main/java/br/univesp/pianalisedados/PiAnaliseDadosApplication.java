a7b442a1e4b68dc18f371e2550c1b81034e965f4
lucasdclopesspd <lucas.lopes@speedyofficer.com.br>
2023-05-10 01:55:06 -0300

    base do projeto

---
+++ src/main/java/br/univesp/pianalisedados/PiAnaliseDadosApplication.java
@@ -0,0 +1,13 @@
+package br.univesp.pianalisedados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiAnaliseDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiAnaliseDadosApplication.class, args);
+	}
+
+}
